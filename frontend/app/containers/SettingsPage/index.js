fa3c01113829ae68886a96d1b01057c5ea98462d
Lui <luih@wit.edu>
2020-06-15 18:30:33 -0400

    add switch button for notifications

--- frontend/app/containers/SettingsPage/index.js
+++ frontend/app/containers/SettingsPage/index.js
@@ -4,6 +4,7 @@
  * List all the settings
  */
 import React from 'react';
+import styled from "styled-components";
 import { Helmet } from 'react-helmet';
 import { FormattedMessage } from 'react-intl';
 import Header from 'components/Header';
@@ -17,6 +18,50 @@ import Img from './Img';
 import Banner from './coolcat.jpg';
 import Ccicon from './creditcardicon.png'
 import './SettingsPage.css';
+
+const CheckBoxWrapper = styled.div`
+  position: relative;
+`;
+const CheckBoxLabel = styled.label`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 42px;
+  height: 26px;
+  border-radius: 15px;
+  background: #bebebe;
+  cursor: pointer;
+  &::after {
+    content: "";
+    display: block;
+    border-radius: 50%;
+    width: 18px;
+    height: 18px;
+    margin: 3px;
+    background: #ffffff;
+    box-shadow: 1px 3px 3px 1px rgba(0, 0, 0, 0.2);
+    transition: 0.2s;
+  }
+`;
+const CheckBox = styled.input`
+  opacity: 0;
+  z-index: 1;
+  border-radius: 15px;
+  width: 42px;
+  height: 26px;
+  &:checked + ${CheckBoxLabel} {
+    background: #4fbe79;
+    &::after {
+      content: "";
+      display: block;
+      border-radius: 50%;
+      width: 18px;
+      height: 18px;
+      margin-left: 21px;
+      transition: 0.2s;
+    }
+  }
+`;
 
 export default function SettingsPage() {
   return (
@@ -79,9 +124,15 @@ export default function SettingsPage() {
           <p class="info-title">
             Notifications
           </p>
-          <p class="info-text" id="creditnumber">
-            Get texts for appointment reminders
-          </p>
+          <div id="payment-container">
+            <p class="info-text" id="creditnumber">
+              Get texts for appointment reminders
+            </p>
+            <CheckBoxWrapper>
+              <CheckBox id="checkbox" type="checkbox" />
+              <CheckBoxLabel htmlFor="checkbox" />
+            </CheckBoxWrapper>
+          </div>
         </div>
 
       </div>
