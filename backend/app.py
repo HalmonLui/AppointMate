71a21af4b87f08664a9419211ddf89a460262932
Parker <parkervr@bu.edu>
2020-06-22 04:51:07 +0000

    embed_map

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,9 @@
-from flask import Flask
+# import app
+from flask import Flask, render_template
 from flask_cors import CORS
+
+# import custom helpers
+from maplib import generate_embed
 
 app = Flask(__name__)
 CORS(app)
@@ -7,9 +11,31 @@ CORS(app)
 # import declared routes
 import frontenddata
 
+
+
+
 @app.route('/')
 def hello():
-    return "Hello World!"
+    location = "850 FOLSOM ST, San Francisco, CA 94107"
+    addresslist = {'a' : generate_embed(location)}
+    return render_template('map.html', addresslist=addresslist )
 
-if __name__ == '__main__':
-    app.run()
+
+
+# def loop_matcher(delay):
+#     while(True):
+#         print('Matcher Automatically Run')
+#         handle_matcher()  
+#         #do expired status update here
+#         time.sleep(delay)
+
+
+# Run Server
+if __name__ == "__main__":
+
+   #matcher_delay = 3600 # 1 hour in seconds
+   #p = Process(target=loop_matcher, args=(matcher_delay,))
+   #p.start()
+
+   app.run(host = '0.0.0.0', debug=True, use_reloader=False)
+   #p.join()
---
+++ backend/maplib.py
@@ -0,0 +1,4 @@
+
+
+def generate_embed(location):
+  return("https://www.google.com/maps/embed/v1/place?key=AIzaSyAivJD4AnACirkXM3_SRsrPxYVXllcXHMM&q="+location.replace(" ", "+"))  
