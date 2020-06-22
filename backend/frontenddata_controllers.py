dfb785499d70deb21b94e3ff3595d595e48e3612
Sonam Ghosh <sonamghosh@gmail.com>
2020-06-22 12:38:06 -0700

    saved endpoints

--- backend/frontenddata_controllers.py
+++ backend/frontenddata_controllers.py
@@ -4,6 +4,7 @@ from square.client import Client
 import pymongo
 import os
 import sys
+import random
 sys.path.insert(1, './discoverpage/')
 sys.path.insert(2, './square-api/customer-api/')
 sys.path.insert(3, './square-api/payments-api/')
@@ -38,17 +39,39 @@ def getSaved():
     db = client["customer"]["customers"]
     """
     SELECT saved_business FROM customers
-    where id = YZSXRJQTCGVSZ7DXGGXNYEY6ER
+    where id = XTGNQH10VCVVS9VBPW61218X0M
     """
-    saved = db.find({"id": "YZSXRJQTCGVSZ7DXGGXNYEY6ER"},
+    # id for Tanjiwou
+    saved = db.find({"id": "XTGNQH10VCVVS9VBPW61218X0M"},
                     {"saved_business": 1, "_id": 0 })
     
     return json.dumps(list(saved), default=str), 200
 
 def addSaved():
+    load_dotenv()
+    client = pymongo.MongoClient(os.getenv("MONGO_NORM_USER"))
+    db = client["customer"]["customers"]
+    """
+    SELECT saved_business FROM customers
+    where id = XTGNQH10VCVVS9VBPW61218X0M
+    """
+    # id for Tanjiwou
+    db2 = client["business"]["2020-06-30"]
+    mongo_data = list(db2.find({}))
+    r_biz = random.sample(mongo_data, 1)
+    data = list(db.find({"id": "XTGNQH10VCVVS9VBPW61218X0M"}))
+    db.update_one({"_id": data[0]["_id"]},
+                   {"$push": {"saved_business": r_biz[0] }})
     return {'success': 'saved item successfully created'}, 200
 
 def removeSaved():
+    load_dotenv()
+    client = pymongo.MongoClient(os.getenv("MONGO_NORM_USER"))
+    db = client["customer"]["customers"]
+    data = list(db.find({"id": "XTGNQH10VCVVS9VBPW61218X0M"}))
+    db.update_one({"_id": data[0]["_id"]},
+                    {"$pop": {"saved_business": 1}})
+
     return {'success', 'saved item successfully removed'}, 200
 
 # BUSINESSES
@@ -76,6 +99,7 @@ def getCards():
     )
 
     customers_api = square_client.customers
+    # id for UwU OwO
     result = customers_api.retrieve_customer("J9XYVXV5Z4SZ77PT2NRN72XFQC")
 
     cards = result.body["customer"]["cards"]
@@ -101,11 +125,12 @@ def removeCard():
     customer_id = "J9XYVXV5Z4SZ77PT2NRN72XFQC"
     card_id = "ccof:8ywRmjQ3OeRVtRjT3GB"
     result = customers_api.delete_customer_card(customer_id, card_id)
-    
+
     return {'success': 'card successfully removed'}, 200
 
 # Customer Creation
 def createCustomer():
+    # new one
     customer = cc.CustomerCreation(
       name="Ai Hayasaka",
       address="750 Post St, San Francisco, CA 94109",
@@ -119,6 +144,7 @@ def createCustomer():
 
 # PAYMENTS
 def sendPayment():
+    # id for Amelia
     payment = pc.PaymentCreation(customer_id="11MVK1PCRRVY1D9QB72HVT64PM",
                                   amount=80,
                                   source_id="ccof:cegcXjydMLiVlDQk3GB",
